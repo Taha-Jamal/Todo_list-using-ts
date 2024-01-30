18997d2270eb0826d6d5e9ae79a1a1da44830376
Taha-Jamal <tjamal618@gmail.com>
2024-01-30 19:12:16 +0500

    first commit

--- dist/index.js
+++ dist/index.js
@@ -5,20 +5,21 @@ function storetext() {
 }
 function showinfo(information) {
     let show = document.getElementById('itemlist');
-    let currentli = document.getElementById('innerlist');
     let circleicon = document.createElement('i');
     let trashicon = document.createElement('i');
+    let Li = document.createElement('li');
     let Span = document.createElement('span');
-    let li = currentli === null || currentli === void 0 ? void 0 : currentli.cloneNode(true);
-    li.innerText = information;
-    show.appendChild(li);
     circleicon.className = 'fa fa-circle text-red-600';
     trashicon.className = 'fa-solid fa-trash cursor-pointer hover:text-red-600';
-    li.insertBefore(Span, li.firstChild);
+    Li.className = 'border-2 rounded-full  h-10 text-white flex justify-between items-center px-2';
+    Span.className = 'text-white lowercase';
+    Span.textContent = information;
+    Li.appendChild(Span);
+    Li.appendChild(trashicon);
+    show.appendChild(Li);
     Span.append(circleicon);
-    li.appendChild(trashicon);
     trashicon.addEventListener('click', function () {
-        show.removeChild(li);
+        show.removeChild(Li);
     });
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,30 @@ function storetext(): string  | void{
 }
 function showinfo(information: string): void {
     let show = document.getElementById('itemlist') as HTMLDataListElement
-    let currentli = document.getElementById('innerlist');
+    // let currentli = document.getElementById('innerlist');
     let circleicon = document.createElement('i')
     let trashicon = document.createElement('i')
+    let Li = document.createElement('li')
     let Span = document.createElement('span')
-    let li = currentli?.cloneNode(true) as HTMLLIElement;
-    
-     li.innerText = information;
-        
-    
-        show.appendChild(li);
-       circleicon.className = 'fa fa-circle text-red-600'
-       trashicon.className = 'fa-solid fa-trash cursor-pointer hover:text-red-600'
-       li.insertBefore(Span, li.firstChild);
-       Span.append(circleicon)
-       li.appendChild(trashicon)
+    circleicon.className = 'fa fa-circle text-red-600'
+    trashicon.className = 'fa-solid fa-trash cursor-pointer hover:text-red-600'
+    Li.className = 'border-2 rounded-full  h-10 text-white flex justify-between items-center px-2'
+    Span.className = 'text-white lowercase'
+    // let li = currentli?.cloneNode(true) as HTMLLIElement;
 
-       trashicon.addEventListener('click', function() {
-        show.removeChild(li)
-    });
+    
+    // Span.appendChild(circleicon)
+    
+    Span.textContent = information;
+    Li.appendChild(Span)
+       
+    Li.appendChild(trashicon)
+    show.appendChild(Li);
+    Span.append(circleicon)
+
+    trashicon.addEventListener('click', function() {
+    show.removeChild(Li)
+});
     
 }
 
